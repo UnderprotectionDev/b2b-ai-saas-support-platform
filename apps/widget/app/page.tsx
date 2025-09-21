fc8b57e88d31ae51a4404339fa02c61399d44711
Underprotection <mertcandemirkaya@outlook.com>
2025-09-21 14:10:51 +0300

    01: Convex Package

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,14 +1,17 @@
-import { add } from "@workspace/math/add";
+"use client";
+
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 3)}</p>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p>apps/widget</p>
+      <Button onClick={() => addUser()}>Add User</Button>
+      <pre> {JSON.stringify(users, null, 2)} </pre>
     </div>
   );
 }
